7f9c6949c5073e570c8f7bec50c55b6a69ca4591
Oleg <hunter553@mail.ru>
2016-11-01 21:29:14 +0200

    calculation

--- Calculate.java
+++ Calculate.java
@@ -2,7 +2,14 @@ public class Calculate {
 
 	public static void main(String[] arg) {
 		System.out.println("Calculate...");
-	
+		int x = Integer.valueOf(arg[0]);
+		int y = Integer.valueOf(arg[1]);
+		System.out.println("Сумма " + (x + y));
+		System.out.println("Разность " + (x - y));
+		System.out.println("Умножение " + (x * y));
+		System.out.println("Деление " + (x / y));
+		System.out.println("Степень " + (Math.pow(x,y)));
+
 	}
 
 }
